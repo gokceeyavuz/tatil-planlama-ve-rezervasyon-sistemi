30eeb6de76261dfcde3ce5abf99cacc6f50fe339
Gökçe Yavuz <118669919+gokceeyavuz@users.noreply.github.com>
2025-03-19 00:20:16 +0300

    dosyaların export edilmesiyle ilgili index.js eklendi

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './src/routes';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import Travel1 from './travel1';
+import Travel2 from './travel2';
+import Travel3 from './travel3';
+import TuruncuBisiklet from './turuncubisiklet';
+import TuruncuBisiklet2 from './turuncuBisiklet2';
+import TuruncuBisiklet3 from './turuncuBisiklet3';
+import AppMor1 from './appMor1';
+import AppMor2 from './appMor2';
+import AppMor3 from './appMor3';
+import FitnessClub from './FitnessClubb';
+import FitnessClub2 from './fitnessClub2';
+import FitnessClub3 from './fitnessClub3';
+import Yayla1 from './yayla1';
+import Yayla2 from './yayla2';
+import Yayla3 from './yayla3';
+export {
+  Travel1,
+  Travel2,
+  Travel3,
+  TuruncuBisiklet,
+  TuruncuBisiklet2,
+  TuruncuBisiklet3,
+  AppMor1,
+  AppMor2,
+  AppMor3,
+  FitnessClub,
+  FitnessClub2,
+  FitnessClub3,
+  Yayla1,
+  Yayla2,
+  Yayla3,
+};
